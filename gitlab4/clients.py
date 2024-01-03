9654823486b6b73c5bda3e6293e4b2f4832c3d35
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-01-03 10:20:12 -0500

    Copied variables.

--- gitlab4/clients.py
+++ gitlab4/clients.py
@@ -4,6 +4,8 @@ from typing import List, Dict
 
 import requests
 
+from gitlab4.schemas import ProjectVariable, Project
+
 
 class GitlabClient:
     def __init__(self, gitlab_token: str):
@@ -19,20 +21,52 @@ class GitlabClient:
             variable_data[variable] = response.json()
         return variable_data
 
-
-    def list_variables(self, project_id: int | str):
+    def list_variables(self, project_id: int | str) -> List[ProjectVariable]:
         """https://docs.gitlab.com/ee/api/project_level_variables.html
         GET /projects/:id/variables
         """
         url = f"{self.host}/api/v4/projects/{project_id}/variables"
-        print(f'Url: {url}')
-        url = "https://gitlab.com/api/v4/projects/42957523/variables"
-        print(f'headers: {self.headers}')
-        # headers = {'PRIVATE-TOKEN': 'glpat-jsfwgbTy_jU8_9hMns1K'}
-        # assert headers == self.headers
         response = requests.get(url=url, headers=self.headers)
         var_data = response.json()
-        return var_data
+        var_list = []
+        if response.status_code == 200:
+            for var in var_data:
+                var_list.append(ProjectVariable(**var))
+        return var_list
+
+    def set_variable(self, project_id: int | str, value: ProjectVariable):
+        """https://docs.gitlab.com/ee/api/project_level_variables.html#update-a-variable
+        TODO Change the return for Pydantic model.
+        """
+        url = f"{self.host}/api/v4/projects/{project_id}/variables/{value.key}"
+        payload = value.model_dump()
+        response = requests.put(url, data=payload, headers=self.headers)
+        print(f'>>>> {response.status_code}')
+        response_data = response.json()
+        return response_data
+
+    def create_variable(self, project_id: int | str, value: ProjectVariable) -> ProjectVariable | None:
+        """https://docs.gitlab.com/ee/api/project_level_variables.html#create-a-variable
+        """
+        url = f"{self.host}/api/v4/projects/{project_id}/variables"
+        payload = value.model_dump()
+        response = requests.post(url, data=payload, headers=self.headers)
+        print(f'>>>> {response.status_code}')
+        if response.status_code == 201:
+            response_data = response.json()
+            project_var = ProjectVariable(**response_data)
+            return project_var
+        else:
+            return None
+
+    def get_project_info(self, project_id: str) -> Project | None:
+        base_url = f"{self.host}/api/v4/projects/{project_id}"
+        response = requests.get(base_url, headers=self.headers)
+        if response.status_code == 200:
+            data = response.json()
+            return Project(**data)
+        else:
+            return None
 
 
 def load_environment_vars():
@@ -44,27 +78,42 @@ def load_environment_vars():
     load_dotenv(dotenv_path=environment_file)
 
 
+def exclude_variables(variables: List[ProjectVariable], value: str = 'ENVIRONMENT') -> List[ProjectVariable]:
+    clean_list = []
+    c = 1
+    i = 1
+    for variable in variables:
+        if value in variable.key:
+            print(f'Excluded {c}. {variable.key}')
+            c += 1
+        else:
+            #  print(f'Added {i}. {variable.key}')
+            i += 1
+            clean_list.append(variable)
+    return clean_list
+
+
 if __name__ == '__main__':
     load_environment_vars()
 
     TOKEN = os.getenv('PRIVATE-TOKEN')
     PROJECT_ID_1 = os.getenv('PROJECT_ID_1')
+    PROJECT_ID_2 = os.getenv('PROJECT_ID_2')
 
     print(f'Token: {TOKEN}')
-    print(f'Project id: {PROJECT_ID_1}')
 
     client = GitlabClient(gitlab_token=TOKEN)
-    response_data = client.list_variables(project_id=PROJECT_ID_1)
+    project_info = client.get_project_info(project_id=PROJECT_ID_1)
+    print(f'Project name: {project_info.name} id: {PROJECT_ID_1} ')
 
-    c = 1
-    i = 1
-    for variable in response_data:
-        if 'ENVIRONMENT' in variable['key']:
-            print(f'-- {c}. {variable["key"]}')
-            c += 1
-        else:
-            print(f'{i}. {variable["key"]}')
-            i += 1
+    project_info = client.get_project_info(project_id=PROJECT_ID_2)
+    print(f'Project name: {project_info.name} id: {PROJECT_ID_2} ')
 
+    project_variables = client.list_variables(project_id=PROJECT_ID_1)
 
+    clean_vars = exclude_variables(variables=project_variables)
 
+    for var in clean_vars:
+        pprint(f'Pushing {var.key}')
+        new_var = client.create_variable(project_id=PROJECT_ID_2, value=var)
+        print(f'Pushed {new_var.key}')
--- gitlab4/schemas.py
+++ gitlab4/schemas.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Optional
+from typing import Any, Optional, List
 
 from pydantic import BaseModel
 
@@ -14,3 +14,166 @@ class ProjectVariable(BaseModel):
     raw: bool
     environment_scope: str
     description: str | None
+
+
+class Namespace(BaseModel):
+    id: int
+    name: str
+    path: str
+    kind: str
+    full_path: str
+    parent_id: Any
+    avatar_url: Any
+    web_url: str
+
+
+class _Links(BaseModel):
+    self: str
+    issues: str
+    merge_requests: str
+    repo_branches: str
+    labels: str
+    events: str
+    members: str
+    cluster_agents: str
+
+
+class ContainerExpirationPolicy(BaseModel):
+    cadence: str
+    enabled: bool
+    keep_n: int
+    older_than: str
+    name_regex: str
+    name_regex_keep: Any
+    next_run_at: str
+
+
+class GroupAccess(BaseModel):
+    access_level: int
+    notification_level: int
+
+
+class Permissions(BaseModel):
+    project_access: Any
+    group_access: GroupAccess
+
+
+class Project(BaseModel):
+    id: int
+    description: Optional[str] = None
+    name: str
+    name_with_namespace: Optional[str] = None
+    path: str
+    path_with_namespace: Optional[str] = None
+    created_at: str
+    default_branch: Optional[str] = None
+    tag_list: Optional[List[str]] = None
+    topics: Optional[List[str]] = None
+    ssh_url_to_repo: Optional[str] = None
+    http_url_to_repo: Optional[str] = None
+    web_url: Optional[str] = None
+    readme_url: Optional[str] = None
+    forks_count: Optional[int] = None
+    avatar_url: Optional[Any] = None
+    star_count: Optional[int] = None
+    last_activity_at: Optional[str] = None
+    namespace: Optional[Namespace] = None
+    container_registry_image_prefix: Optional[str] = None
+    _links: Optional[_Links] = None
+    code_suggestions: Optional[bool] = None
+    packages_enabled: Optional[bool] = None
+    empty_repo: Optional[bool] = None
+    archived: Optional[bool] = None
+    visibility: Optional[str] = None
+    resolve_outdated_diff_discussions: Optional[bool] = None
+    container_expiration_policy: Optional[ContainerExpirationPolicy] = None
+    issues_enabled: Optional[bool] = None
+    merge_requests_enabled: Optional[bool] = None
+    wiki_enabled: Optional[bool] = None
+    jobs_enabled: Optional[bool] = None
+    snippets_enabled: Optional[bool] = None
+    container_registry_enabled: Optional[bool] = None
+    service_desk_enabled: Optional[bool] = None
+    service_desk_address: Optional[str] = None
+    can_create_merge_request_in: Optional[bool] = None
+    issues_access_level: Optional[str] = None
+    repository_access_level: Optional[str] = None
+    merge_requests_access_level: Optional[str] = None
+    forking_access_level: Optional[str] = None
+    wiki_access_level: Optional[str] = None
+    builds_access_level: Optional[str] = None
+    snippets_access_level: Optional[str] = None
+    pages_access_level: Optional[str] = None
+    analytics_access_level: Optional[str] = None
+    container_registry_access_level: Optional[str] = None
+    security_and_compliance_access_level: Optional[str] = None
+    releases_access_level: Optional[str] = None
+    environments_access_level: Optional[str] = None
+    feature_flags_access_level: Optional[str] = None
+    infrastructure_access_level: Optional[str] = None
+    monitor_access_level: Optional[str] = None
+    model_experiments_access_level: Optional[str] = None
+    model_registry_access_level: Optional[str] = None
+    emails_disabled: Optional[bool] = None
+    emails_enabled: Optional[bool] = None
+    shared_runners_enabled: Optional[bool] = None
+    lfs_enabled: Optional[bool] = None
+    creator_id: Optional[int] = None
+    import_url: Optional[Any] = None
+    import_type: Optional[Any] = None
+    import_status: Optional[str] = None
+    import_error: Optional[Any] = None
+    open_issues_count: Optional[int] = None
+    description_html: Optional[str] = None
+    updated_at: Optional[str] = None
+    ci_default_git_depth: Optional[int] = None
+    ci_forward_deployment_enabled: Optional[bool] = None
+    ci_forward_deployment_rollback_allowed: Optional[bool] = None
+    ci_job_token_scope_enabled: Optional[bool] = None
+    ci_separated_caches: Optional[bool] = None
+    ci_allow_fork_pipelines_to_run_in_parent_project: Optional[bool] = None
+    build_git_strategy: Optional[str] = None
+    keep_latest_artifact: Optional[bool] = None
+    restrict_user_defined_variables: Optional[bool] = None
+    runners_token: Optional[str] = None
+    runner_token_expiration_interval: Optional[Any] = None
+    group_runners_enabled: Optional[bool] = None
+    auto_cancel_pending_pipelines: Optional[str] = None
+    build_timeout: Optional[int] = None
+    auto_devops_enabled: Optional[bool] = None
+    auto_devops_deploy_strategy: Optional[str] = None
+    ci_config_path: Optional[str] = None
+    public_jobs: Optional[bool] = None
+    shared_with_groups: Optional[List] = None
+    only_allow_merge_if_pipeline_succeeds: Optional[bool] = None
+    allow_merge_on_skipped_pipeline: Optional[Any] = None
+    request_access_enabled: Optional[bool] = None
+    only_allow_merge_if_all_discussions_are_resolved: Optional[bool] = None
+    remove_source_branch_after_merge: Optional[bool] = None
+    printing_merge_request_link_enabled: Optional[bool] = None
+    merge_method: Optional[str] = None
+    squash_option: Optional[str] = None
+    enforce_auth_checks_on_uploads: Optional[bool] = None
+    suggestion_commit_message: Optional[Any] = None
+    merge_commit_template: Optional[Any] = None
+    squash_commit_template: Optional[Any] = None
+    issue_branch_template: Optional[Any] = None
+    autoclose_referenced_issues: Optional[bool] = None
+    approvals_before_merge: Optional[int] = None
+    mirror: Optional[bool] = None
+    external_authorization_classification_label: Optional[str] = None
+    marked_for_deletion_at: Optional[Any] = None
+    marked_for_deletion_on: Optional[Any] = None
+    requirements_enabled: Optional[bool] = None
+    requirements_access_level: Optional[str] = None
+    security_and_compliance_enabled: Optional[bool] = None
+    compliance_frameworks: Optional[List] = None
+    issues_template: Optional[Any] = None
+    merge_requests_template: Optional[Any] = None
+    ci_restrict_pipeline_cancellation_role: Optional[str] = None
+    merge_pipelines_enabled: Optional[bool] = None
+    merge_trains_enabled: Optional[bool] = None
+    merge_trains_skip_train_allowed: Optional[bool] = None
+    allow_pipeline_trigger_approve_deployment: Optional[bool] = None
+    permissions: Optional[Permissions] = None
+
