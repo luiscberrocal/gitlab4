f044c39495810d689313adea938f4448d60a34e6
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2024-01-04 09:50:51 -0500

    Parsing files.

--- gitlab4/environments/files.py
+++ gitlab4/environments/files.py
@@ -1,7 +1,8 @@
 import base64
 import os
+import re
 from pathlib import Path
-from typing import Callable, List
+from typing import Callable, List, Dict, Any
 
 from rich.pretty import pprint
 
@@ -35,7 +36,21 @@ def save_project_var(variable: ProjectVariable, file_path: Path, encryption_func
     return var_file
 
 
-def save_multiple_variables(folder: Path, environment_vars:List[str]):
+def parse_environment_file(file_path: Path) -> Dict[str, Any]:
+    var_regexp_str = re.compile(r"^(?P<var_name>[A-Z0-9_]+)\s*=\s*(?P<var_value>.*)")
+    var_regexp = re.compile(var_regexp_str)
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+    var_dict = {}
+    for line in lines:
+        line = line.strip('\n')
+        match = var_regexp.match(line)
+        if match:
+            var_dict[match.group('var_name')] = match.group('var_value')
+    return var_dict
+
+
+def save_multiple_variables(folder: Path, environment_vars: List[str]):
     for env in environment_vars:
         var = client.get_variable(project_id=PROJECT_ID, variable_name=env.strip())
 
@@ -44,8 +59,22 @@ def save_multiple_variables(folder: Path, environment_vars:List[str]):
         print(var_file)
 
 
-if __name__ == '__main__':
+def set_files_to_variables(folder: Path, pattern: str):
+    txt_files = folder.glob(pattern)
+    for txt_file in txt_files:
+        print(txt_file.stem)
+        push = input(f'Push {txt_file} to {project_info.name}: ')
+        if push.lower() == 'y':
+            with open(txt_file, 'r') as f:
+                clear_value = f.read()
+            encrypted_content = encode(clear_value, 'utf-8')
+            project_var = ProjectVariable(key=txt_file.stem, value=encrypted_content)
+            pprint(project_var)
+            print('-' * 70)
+            client.set_variable(project_id=PROJECT_ID, value=project_var)
 
+
+if __name__ == '__main__':
     load_environment_vars()
 
     TOKEN = os.getenv('PRIVATE-TOKEN')
@@ -61,15 +90,16 @@ if __name__ == '__main__':
     output_folder = Path(__file__).parent.parent.parent / 'output'
     print(output_folder, output_folder.exists())
     read_folder = output_folder / project_info.path
-    txt_files = read_folder.glob('*_STAGING.txt')
-    for txt_file in txt_files:
-        print(txt_file.stem)
-        with open(txt_file, 'r') as f:
-            clear_value = f.read()
-        encrypted_content = encode(clear_value, 'utf-8')
-        project_var = ProjectVariable(key=txt_file.stem, value=encrypted_content)
-        pprint(project_var)
-        print('-'*70)
-        client.create_variable(project_id=PROJECT_ID, value=project_var)
+    # file_pattern ='*_STAGING.txt'
+    # set_files_to_variables(folder=read_folder, pattern=file_pattern)
 
     # save_multiple_variables()
+    staging_django_file = read_folder / 'ENVIRONMENT_DJANGO_STAGING.txt'
+    staging_vars_dict = parse_environment_file(staging_django_file)
+    pprint(staging_vars_dict)
+
+    prod_django_file = read_folder / 'ENVIRONMENT_DJANGO_PRODUCTION.txt'
+    prod_vars_dict = parse_environment_file(staging_django_file)
+    pprint(prod_vars_dict)
+
+    print(set(prod_vars_dict.keys()) == set(staging_vars_dict.keys()))
